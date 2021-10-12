8d589859b9c800c28fe1b028c2e7e868a997ef5e
Imanub5 <87994343+Imanub5@users.noreply.github.com>
2021-10-12 13:57:08 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,21 +1,32 @@
-from flask import Flask, render_template, session, redirect
+from flask import Flask, render_template, request, redirect
 
-app = Flask(__name__)
+from user import User
 
-app.secret_key="Benny bob wuz heer."
+app=Flask(__name__)
 
 @app.route('/')
 def index():
-    if "count" not in session:
-        session["count"] = 0
-    else:
-        session['count'] += 1
-    return render_template("Counter.html")
+    return redirect('/users')
 
-@app.route('/reset')
-def reset():
-    session.clear()
-    return redirect('/')
+
+@app.route('/users')
+def users():
+    return render_template("users.html",users=User.get_all())
+
+
+@app.route('/user/new')
+def new():
+    return render_template("new_user.html")
+
+@app.route('/user/create',methods=['POST'])
+def create():
+    print(request.form)
+    User.save(request.form)
+    return redirect('/users')
+
+
+
+
 
 if __name__=="__main__":
     app.run(debug=True)
